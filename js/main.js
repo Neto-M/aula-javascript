c8bb6992e4c24003a41ecdc04d5c9abf25a6a8f2
Neto-M <neto83454@gmail.com>
2021-03-21 22:50:15 -0300

    adicionando comentarios

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 alert("Meu primeiro js")
+
+/*var frutas = [{nome:"Maça", cor:"Vermelho"}, {nome:"Uva", cor:"Roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+
+//var fruta = {nome:"maça", cor:"Vermelha"};
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+
+
+//var lista = ["maça", "Pêra", "Laranja"]
+//lista.push("Uva");
+//lista.pop();
+
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+
+/*
+var nome = "Manoel Neto";
+var n1 = 7;
+var n2 = 8;
+var frase = "Japão é o melhor time do mundo";
+alert(nome + "tem" + idade + "anos");
+alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+alert(frase.replace("Japão", "Brasil"));
+*/
